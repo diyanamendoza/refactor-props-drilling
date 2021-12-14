66fa7179d5a5784496ce575af6015f7758e34154
diyanamendoza <diyana.amp@pm.me>
2021-12-13 16:29:43 -0800

    Move useEffect to context file

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,21 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
+import { fetchUser } from "../services/user";
 
 const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState('')
+
+    useEffect(() => {
+        fetchUser()
+          .then((fetchedUser) => {
+            setUser(fetchedUser)
+          })
+          .catch((error) => {
+            throw new Error(`Error: ${error}`)
+          })
+      }, [])
+    
     return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
 }
 
